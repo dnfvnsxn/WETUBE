15ba8d270f33ba212fa816a67b952c4d103573dd
dnfvnsxn <dnfvnsxn@naver.com>
2019-04-03 22:57:15 +0900

    Getting Video by ID

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
-import { localsMiddleware } from "./middlewares";
+import {
+  localsMiddleware
+} from "./middlewares";
 const app = express();
 
 app.use(helmet());
@@ -15,7 +17,9 @@ app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(morgan("dev"));
 app.use(localsMiddleware);
 
